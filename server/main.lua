4a4fc8bec7ddf021f588b7ccf248c89a09561fb5
Nix <93496101+NixCD@users.noreply.github.com>
2022-02-21 18:07:37 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,7 +1,3 @@
-ESX = nil
-
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
 Citizen.CreateThread(function()
     if true then
         print('Loaded Correctly :D. - Script Created by Nix#7410')
